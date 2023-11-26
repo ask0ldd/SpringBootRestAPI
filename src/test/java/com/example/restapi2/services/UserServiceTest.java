c3f9aa1553e545cce3492a99e492757bf0247209
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-26 23:05:47 +0100

    unit test : user service : getusers

--- src/test/java/com/example/restapi2/services/UserServiceTest.java
+++ src/test/java/com/example/restapi2/services/UserServiceTest.java
@@ -2,6 +2,9 @@ package com.example.restapi2.services;
 
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -38,7 +41,7 @@ public class UserServiceTest {
 
     @Test
     @DisplayName("User exists : .getUser(id) should return the expected User")
-    public void getUser() {
+    public void getUser_ReturnOneUser() {
 
         when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(user1));
 
@@ -50,6 +53,67 @@ public class UserServiceTest {
         Assertions.assertThat(collectedUser.get().getLastname()).isEqualTo(user1.getLastname());
         Assertions.assertThat(collectedUser.get().getPassword()).isEqualTo(user1.getPassword());
         Assertions.assertThat(collectedUser.get().getEmail()).isEqualTo(user1.getEmail());
+    }
+
+    /*
+     * @Test
+     * 
+     * @DisplayName("User exists : .updateUser(id) should return the expected User")
+     * public void UpdateUser() {
+     * 
+     * when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(user1));
+     * 
+     * Optional<User> collectedUser = userService.getUser(1L);
+     * 
+     * Assertions.assertThat(collectedUser.isPresent()).isTrue();
+     * Assertions.assertThat(collectedUser.get().getUserId()).isGreaterThan(0);
+     * Assertions.assertThat(collectedUser.get().getFirstname()).isEqualTo(user1.
+     * getFirstname());
+     * Assertions.assertThat(collectedUser.get().getLastname()).isEqualTo(user1.
+     * getLastname());
+     * Assertions.assertThat(collectedUser.get().getPassword()).isEqualTo(user1.
+     * getPassword());
+     * Assertions.assertThat(collectedUser.get().getEmail()).isEqualTo(user1.
+     * getEmail());
+     * 
+     * userService.saveUser(new User(1L, "Laurentine", "GINA",
+     * "laurentinegina@mail.com", "laurent"));
+     * 
+     * Optional<User> postUpdateCollectedUser = userService.getUser(1L);
+     * 
+     * Assertions.assertThat(postUpdateCollectedUser.isPresent()).isTrue();
+     * Assertions.assertThat(postUpdateCollectedUser.get().getFirstname()).isEqualTo
+     * ("Laurentine");
+     * Assertions.assertThat(postUpdateCollectedUser.get().getEmail()).isEqualTo(
+     * "laurentinegina@mail.com");
+     * }
+     */
+
+    @Test
+    @DisplayName("User exists : .getUsers(id) should return all the existing User")
+    public void getUsers_ReturnAllUsers() {
+
+        ArrayList<User> userCollection = new ArrayList<>();
+        userCollection.add(user1);
+        userCollection.add(user2);
+        userCollection.add(user2);
+
+        when(userRepository.findAll()).thenReturn((Iterable<User>) userCollection);
+
+        Iterable<User> collectedUsers = userService.getUsers();
+        Iterator<User> it = collectedUsers.iterator();
+        // collectedUsers.forEach(null);
+
+        int i = 0;
+        while (it.hasNext()) {
+            User itUser = it.next();
+            Assertions.assertThat(itUser.getUserId()).isGreaterThan(0);
+            Assertions.assertThat(itUser.getFirstname()).isEqualTo(userCollection.get(i).getFirstname());
+            Assertions.assertThat(itUser.getLastname()).isEqualTo(userCollection.get(i).getLastname());
+            Assertions.assertThat(itUser.getPassword()).isEqualTo(userCollection.get(i).getPassword());
+            Assertions.assertThat(itUser.getEmail()).isEqualTo(userCollection.get(i).getEmail());
+            i++;
+        }
     }
 
 }
